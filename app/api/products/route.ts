413999c716876cd5b0b1fc5a297b7307dba86f1f
Pavan <pavannarindi@gmail.com>
2026-05-25 06:58:07 +0530

    Remove Prisma 7 config

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,32 +1,56 @@
+export const dynamic = "force-dynamic";
+
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
 
-  const products = await prisma.product.findMany({
-    include: {
-      inventories: {
-        include: {
-          warehouse: true,
+  const products =
+    await prisma.product.findMany({
+
+      include: {
+        inventories: {
+          include: {
+            warehouse: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  const formatted = products.map((product) => ({
-    id: product.id,
-    name: product.name,
-    description: product.description,
+  const formatted =
+    products.map((product) => ({
 
-    inventories: product.inventories.map((inv) => ({
-      warehouse: inv.warehouse.name,
-      totalStock: inv.totalStock,
-      reservedStock: inv.reservedStock,
-      availableStock:
-        inv.totalStock - inv.reservedStock,
-      warehouseId: inv.warehouseId,
-    })),
-  }));
+      id: product.id,
 
-  return NextResponse.json(formatted);
+      name: product.name,
+
+      description:
+        product.description,
+
+      inventories:
+        product.inventories.map(
+          (inv) => ({
+
+            warehouse:
+              inv.warehouse.name,
+
+            totalStock:
+              inv.totalStock,
+
+            reservedStock:
+              inv.reservedStock,
+
+            availableStock:
+              inv.totalStock -
+              inv.reservedStock,
+
+            warehouseId:
+              inv.warehouseId,
+          })
+        ),
+    }));
+
+  return NextResponse.json(
+    formatted
+  );
 }
